e1006968a609fc0be5a3e23d82c11e3aef7996f2
Xavier K <xavierkhew00@gmail.com>
2021-08-26 22:27:25 +0800

    pill's size change on mobile responsiveness

--- components/NavLink.js
+++ components/NavLink.js
@@ -5,7 +5,7 @@ export default function NavLink({ href, name }) {
         <Link href={href}>
             <a target='_blank'>
                 <div className='bg-[color:var(--myred)] rounded-3xl text-2xl mb-5 w-[95%] m-auto py-2 text-gray-200
-                md:text-3xl md:mb-8 md:w-[30%]'>
+                md:text-3xl md:mb-8 lg:w-[30%]'>
                     {name}
                 </div>
             </a>
