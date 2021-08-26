62ee546367467161098a46957614aaa102f045a6
Xavier K <xavierkhew00@gmail.com>
2021-08-26 20:22:38 +0800

    setup styles

---
+++ globalvariables.js
@@ -0,0 +1,5 @@
+export const links = [
+    { name: 'github', href: 'https://github.com/xk2800' },
+    { name: 'linkedin', href: 'https://linkedin.com/in/plaxamana' },
+    { name: 'website', href: 'https://xavierkhew.tech' },
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: 'jit',
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                main: ['Lexend Deca'],
+
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
