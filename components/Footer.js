c8ad72bf4e93f238d516dc3df82a1d258fdcf7e5
Xavier K <xavierkhew00@gmail.com>
2021-08-26 21:37:22 +0800

    change footer font size

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="mt-16">
+        <div className="mt-16 text-[18px]">
             Designed & Built with &#10084;&#65039; by Xavier K. in Subang Jaya.
         </div>
     )
