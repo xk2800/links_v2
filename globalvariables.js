deabf46f2bd3978f8677e1f2b5010fb558764f08
Restyled.io <commits@restyled.io>
2021-12-22 13:16:42 +0000

    Restyled by clang-format

--- globalvariables.js
+++ globalvariables.js
@@ -1,7 +1,7 @@
 export const links = [
-    { name: 'My Website1', href: 'https://xavierkhew.tech' },
-    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/xavier-khew/' },
-    { name: 'Github', href: 'https://github.com/xk2800' },
-    { name: 'Email Me', href:'mailto:contact.xavierk@gmail.com'}
-    
+  {name : 'My Website1', href : 'https://xavierkhew.tech'},
+  {name : 'LinkedIn', href : 'https://www.linkedin.com/in/xavier-khew/'},
+  {name : 'Github', href : 'https://github.com/xk2800'},
+  {name : 'Email Me', href : 'mailto:contact.xavierk@gmail.com'}
+
 ]
