5eacc6f0c6f6391456cc4dfbed4210686f32815b
Restyled.io <commits@restyled.io>
2021-12-22 12:41:12 +0000

    Restyled by prettier

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function helloAPI(req, res) {
-  res.status(200).json({Message : "Hello! there's no API here."})
+  res.status(200).json({ Message: "Hello! there's no API here." });
 }
