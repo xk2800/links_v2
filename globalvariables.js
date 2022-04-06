4210e58ea0219a984525c8c14b12bf39c89baf18
Xavier K <59243836+xk2800@users.noreply.github.com>
2022-04-06 20:22:26 +0800

    Update globalvariables.js

--- globalvariables.js
+++ globalvariables.js
@@ -1,5 +1,5 @@
 export const links = [
-    { name: 'My Website', href: 'https://xavierkhew.tech' },
+    { name: 'My Website', href: 'https://xk2800.github.io' },
     { name: 'LinkedIn', href: 'https://www.linkedin.com/in/xavier-khew/' },
     { name: 'Github', href: 'https://github.com/xk2800' },
     { name: 'Email Me', href:'mailto:contact.xavierk@gmail.com'}
