4904b9f106a45f11b14dfc3506731b4b69df2e6a
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-02-06 20:33:54 +0800

    update link troll

--- components/Footer.js
+++ components/Footer.js
@@ -4,11 +4,11 @@ const todaysDate = new Date();
 const currentYear = todaysDate.getFullYear();
 
 export default function Footer() {
-    return (
-        <div className="mt-[40px] px-[60px] text-[16px] md:mt-16 md:px-0 md:text-[18px] leading-7">
-            Designed & Built with <Link href="https://hello.xavierkhew.tech/"><a className="animate-pulse">&#10084;&#65039;</a></Link>  by Xavier K.
-            <br/>
-            in Subang Jaya &#169; {currentYear}
-        </div>
-    )
+  return (
+    <div className="mt-[40px] px-[60px] text-[16px] md:mt-16 md:px-0 md:text-[18px] leading-7">
+      Designed & Built with <Link href="https://xavierkhew.com/fun"><a className="animate-pulse">&#10084;&#65039;</a></Link>  by Xavier K.
+      <br />
+      in Subang Jaya &#169; {currentYear}
+    </div>
+  );
 }
